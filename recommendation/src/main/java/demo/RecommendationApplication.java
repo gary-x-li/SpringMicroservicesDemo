8296eafcc0ddc6ef3f1d474e2a2fd6e2c9555015
Gary Li <xgng_l@yahoo.com>
2017-10-09 00:08:18 -0400

    Tabs to Spaces

--- recommendation/src/main/java/demo/RecommendationApplication.java
+++ recommendation/src/main/java/demo/RecommendationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecommendationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecommendationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationApplication.class, args);
+    }
 }
