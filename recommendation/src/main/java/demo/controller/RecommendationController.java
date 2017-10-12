7db2dc9cc6b85bec30f31468a899fce7fba1649a
Gary Li <xgng_l@yahoo.com>
2017-10-11 20:59:52 -0400

    Add RefreshScope and Change Spring Boot and Cloud versions

--- recommendation/src/main/java/demo/controller/RecommendationController.java
+++ recommendation/src/main/java/demo/controller/RecommendationController.java
@@ -1,12 +1,14 @@
 package demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="users/{userId}")
+@RefreshScope
 public class RecommendationController {
 
     @Value("${example.property}")
